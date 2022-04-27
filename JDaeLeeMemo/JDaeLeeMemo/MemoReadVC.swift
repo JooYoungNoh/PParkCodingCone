ce310b5f7d817d4dd2f32d268c3214499797c483
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-28 06:50:51 +0900

    fix : 옵셔널 값 수정

--- JDaeLeeMemo/JDaeLeeMemo/MemoReadVC.swift
+++ JDaeLeeMemo/JDaeLeeMemo/MemoReadVC.swift
@@ -22,13 +22,13 @@ class MemoReadVC: UIViewController {
         self.contents.text = param?.contents
         
         //문장 수
-        self.numberLabel.text = "Number of characters : \(String(describing: param?.writelength))"
+        self.numberLabel.text = "Number of characters : \(String(describing: (param?.writelength)!))"
         
         //에디트 액션 버튼으로 가야됨
         //비밀 여부
         if param?.secret == true {
             self.secretSwich.isOn = true
-            self.secretLabel.text = "Secret Activation \n" + "비밀번호: \(param?.number)"
+            self.secretLabel.text = "Secret Activation \n" + "비밀번호: \((param?.number)!)"
             self.secretLabel.textColor = UIColor.blue
         
         } else {
