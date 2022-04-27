ea967c04f957a6a278107aea3c53fbb24e0fdf0a
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-28 06:21:29 +0900

    add : 아울렛 변수 선언 및 연결

--- JDaeLeeMemo/JDaeLeeMemo/MemoReadVC.swift
+++ JDaeLeeMemo/JDaeLeeMemo/MemoReadVC.swift
@@ -8,22 +8,14 @@
 import UIKit
 
 class MemoReadVC: UIViewController {
-
+    
+    @IBOutlet weak var contents: UITextView!
+    @IBOutlet weak var secretSwich: UISwitch!
+    @IBOutlet weak var secretLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
