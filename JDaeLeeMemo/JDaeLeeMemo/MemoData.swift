dd3b2bef38c95cb7723148c1831ce7392b3277f7
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-28 04:33:16 +0900

    add : 메모 작성 시 데이터를 저장할 파일 및 배열 변수 생성

---
+++ JDaeLeeMemo/JDaeLeeMemo/MemoData.swift
@@ -0,0 +1,16 @@
+//
+//  MemoData.swift
+//  JDaeLeeMemo
+//
+//  Created by 노주영 on 2022/04/28.
+//
+
+import UIKit
+
+class MemoData{
+    var memoIdx: Int?
+    var title: String?
+    var contents: String?
+    var regdate: Date?
+    
+}
